80ecee63609b3a433aff1ea8d119df1537e1186b
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-01-22 17:24:48 -0300

    Update database.sh

--- database.sh
+++ database.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 # description: Starts and stops the Oracle listener and database
+
+
+#Carrega o Profile
 source /u01/scripts/profiles/CDBRMSCO.sh
+
+#Variavel para ver se o banco está aberto
+net=$(netstat -an | grep LISTEN | grep 1521)
 
 case "$1" in
     'start')
-        # Start the listener:
-        lsnrctl start
+        #Verifica se o Banco está aberto e passa uma mensagem
+        if echo "$net" = | grep -q "tcp6.*0.*:::1521.*:::.*LISTEN"; then
+            echo "Banco já está aberto"
+        else
+            #Inicia o Banco
+            # Start the listener:
+            lsnrctl start
 
-        # Start the databases:
-        sqlplus '/ as sysdba' <<'EOF'
-            startup
-            exit
+            # Start the databases:
+            sqlplus '/ as sysdba' <<'EOF'
+                startup
+                exit
 EOF
+        fi
         ;;
     'stop')
         # Stop the listener:
@@ -22,5 +34,9 @@ EOF
             shutdown immediate
             exit
 EOF
+        ;;
+    *)
+        # Mensagem de erro:
+        echo "Não foi passado nenhum parâmetro, escreva start ou stop"
         ;;
 esac
